d6df20367b0b80ec8ce1672109807914b0defbcd
shosokaw <shosokaw@C010423556.zipangu.cac.co.jp>
2026-05-11 10:34:40 +0900

    lesson08.09完了 100本ノック完了

---
+++ Java100_questions_cac2nd/src/lesson13_14/challenge03/Nurserry/logic/NurserySchoolChild.java
@@ -0,0 +1,23 @@
+package lesson13_14.challenge03.Nurserry.logic;
+
+public class NurserySchoolChild {
+
+	private String name;
+
+	public NurserySchoolChild(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	void Play() {
+		System.out.println("遊ぶよ～。");
+	}
+
+}
